4b7b2bcb96b8e7e24491f11d4a3789460944490f
Resch416 <esch.ryla@gmail.com>
2021-01-28 18:46:47 -0600

    Final edits made to R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,59 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix() creates a list of funtions that serve as the input for cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## @x: a square invertible matrix
-  ## return: a list containing functions to
-  ##              1. set the matrix
-  ##              2. get the matrix
-  ##              3. set the inverse
-  ##              4. get the inverse
-  ##         this list is used as the input to cacheSolve()
-  
-  inv = NULL
-  set = function(y) {
-    # use `<<-` to assign a value to an object in an environment 
-    # different from the current environment. 
+  inv <- NULL
+  set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get = function() x
-  setinv = function(inverse) inv <<- inverse 
-  getinv = function() inv
-  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## cacheSolve() checks whether or not the inverse of the matrix has already been calculated
+## This action relies heavily on the rules of S3 objects
+## If the inverse has not already been calculated, solve() is employed to do so
+## The solution is then added to the vector 'inv', an object in the makeCacheMatrix() env
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  ## @x: output of makeCacheMatrix()
-  ## return: inverse of the original matrix input to makeCacheMatrix()
-  
-  inv = x$getinv()
-  
-  # if the inverse has already been calculated
-  if (!is.null(inv)){
-    # get it from the cache and skips the computation. 
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  
-  # otherwise, calculates the inverse 
-  else {
-    mat.data = x$get()
-    inv = solve(mat.data, ...)
-    
-    # sets the value of the inverse in the cache via the setinv function.
-    x$setinv(inv)
-    
-    return(inv)
-  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
-
-## Computing the inverse of a square matrix can be done with the solve function in R.
-## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
-## For this assignment, assume that the matrix supplied is always invertible
